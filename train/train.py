c1a97ad6ab778594ef127b1737baf6b03db60137
Berat Denizdurduran <bdenizdurduran@gmail.com>
2022-09-21 09:55:30 +0200

    FIX > train is fixed

--- train/train.py
+++ train/train.py
@@ -1,75 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
-
-@denizdurduran
-pytorch
-/
-tutorials
-Public
-Code
-Issues
-177
-Pull requests
-61
-Actions
-Projects
-Security
-Insights
-tutorials/beginner_source/blitz/cifar10_tutorial.py /
-@wesleyacheng
-wesleyacheng upgrade to f-strings (#1753)
-…
-Latest commit 563162a on Jan 4
- History
- 23 contributors
-@holly1238@chsasank@wesleyacheng@yf225@vra@jspisak@soumith@SimonGDunne@rht@orpheuslummis@JoelMarcey@ikamensh
-367 lines (293 sloc)  12.5 KB
-
-# -*- coding: utf-8 -*-
-"""
-Training a Classifier
-=====================
-This is it. You have seen how to define neural networks, compute loss and make
-updates to the weights of the network.
-Now you might be thinking,
-What about data?
-----------------
-Generally, when you have to deal with image, text, audio or video data,
-you can use standard python packages that load data into a numpy array.
-Then you can convert this array into a ``torch.*Tensor``.
--  For images, packages such as Pillow, OpenCV are useful
--  For audio, packages such as scipy and librosa
--  For text, either raw Python or Cython based loading, or NLTK and
-   SpaCy are useful
-Specifically for vision, we have created a package called
-``torchvision``, that has data loaders for common datasets such as
-ImageNet, CIFAR10, MNIST, etc. and data transformers for images, viz.,
-``torchvision.datasets`` and ``torch.utils.data.DataLoader``.
-This provides a huge convenience and avoids writing boilerplate code.
-For this tutorial, we will use the CIFAR10 dataset.
-It has the classes: ‘airplane’, ‘automobile’, ‘bird’, ‘cat’, ‘deer’,
-‘dog’, ‘frog’, ‘horse’, ‘ship’, ‘truck’. The images in CIFAR-10 are of
-size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
-.. figure:: /_static/img/cifar10.png
-   :alt: cifar10
-   cifar10
-Training an image classifier
-----------------------------
-We will do the following steps in order:
-1. Load and normalize the CIFAR10 training and test datasets using
-   ``torchvision``
-2. Define a Convolutional Neural Network
-3. Define a loss function
-4. Train the network on the training data
-5. Test the network on the test data
-1. Load and normalize CIFAR10
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-Using ``torchvision``, it’s extremely easy to load CIFAR10.
-"""
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -89,12 +17,12 @@ transform = transforms.Compose(
 
 batch_size = 4
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+trainset = torchvision.datasets.CIFAR10(root='./code', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+testset = torchvision.datasets.CIFAR10(root='./code', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
